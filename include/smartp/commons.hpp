04a5becc5ffcb72bd7d05fb54b2e34aa57cee1e8
Afalide <afalide@hotmail.fr>
2015-06-17 14:45:29 +0200

    adds #defines for specific windows __declspec inetrface

--- include/smartp/commons.hpp
+++ include/smartp/commons.hpp
@@ -2,16 +2,28 @@
 #ifndef SMARTP_COMMONS_HPP
 #define SMARTP_COMMONS_HPP
 
-#ifdef SMARTP_BUILDING_LIB
-#define SMARTP_API __declspec(dllexport)
+///////////////////////////////////////////
+
+//windows __declspec for dlls
+
+#ifdef WIN32
+    #ifdef SMARTP_BUILDING_LIB
+        #define SMARTP_API __declspec(dllexport)
+    #else
+        #define SMARTP_API __declspec(dllimport)
+    #endif
 #else
-#define SMARTP_API __declspec(dllimport)
+    #define SMARTP_API
 #endif
 
+///////////////////////////////////////////
+
+// debug outputs
+
 #ifdef SMARTP_USE_LOG
-#define SMARTP_LOG(X) X;
+    #define SMARTP_LOG(X) X;
 #else
-#define SMARTP_LOG(X)
+    #define SMARTP_LOG(X)
 #endif
 
 #endif //SMARTP_COMMONS_HPP
